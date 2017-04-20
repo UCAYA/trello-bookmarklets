a74634f73adb9e224b61d4f2676b97ab06b14db7
Romuald Boulanger <romuald.boulanger@ucaya.com>
2017-04-20 13:57:22 +0200

    Replace native prompt with SweetAlert2

--- releasenotes-to-clipboard/releasenotes-to-clipboard.js
+++ releasenotes-to-clipboard/releasenotes-to-clipboard.js
@@ -57,89 +57,127 @@ javascript:(function(){
 	$.get('/1/boards/' + idBoard + '/lists', { cards: 'open', card_fields: 'url,name,labels' })
 	.success(function(json){
 
-	  var listName = prompt('List ?', 'Done');
-	  var list = json.find(function(_){ return _.name.toLowerCase() === listName.toLowerCase() });
+		var listsSelector = '<select id="swal-list" class="swal2-input">';
+		for (var i = 0; i < json.length; i++) {
+			var l = json[i];
+			var selectedAttr = l.name.toLowerCase() === 'done' ? 'selected="selected"' : '';
+			listsSelector += '<option value="' + l.name + '" ' + selectedAttr + '>' + l.name + '</option>';
+		}
+		listsSelector += '</select>';
 
-		sb.push('# 📦 ' + new Date().toLocaleString());
-		sb.push('\n');
-		sb.push('## ' + prompt('Version ?', ''));
-		sb.push('\n');
+		swal({
+			type: 'question',
+		  title: 'Release Notes',
+			confirmButtonText: '🖨 Go!',
+		  html:
+		    '🗃 List?' +
+				listsSelector +
+		    '📦 Version?' +
+				'<input id="swal-version" class="swal2-input" placeholder="Build number">',
+		  preConfirm: function () {
+		    return new Promise(function (resolve) {
+		      resolve([
+		        $('#swal-list').val(),
+		        $('#swal-version').val()
+		      ])
+		    })
+		  },
+		  onOpen: function () {
+		    $('#swal-list').focus()
+		  }
+		}).then(function (result) {
 
-		//find "⭐️ New"
-		var cards = list.cards.filter(isNew);
+			var listName = result[0];
+			var versionName = result[1];
+			console.log(listName);
+			console.log(versionName);
 
-		if(cards && cards.length > 0) {
-			console.log('⭐️ New: ' + cards.length);
+			var list = json.find(function(_){ return _.name.toLowerCase() === listName.toLowerCase() });
 
-			sb.push('## ⭐️ New');
+			sb.push('# 📦 ' + new Date().toLocaleString());
+			sb.push('\n');
+			sb.push('## ' + versionName);
+			sb.push('\n');
 
-			for (var i = 0; i < cards.length; i++) {
-				var c = cards[i];
+			//find "⭐️ New"
+			var cards = list.cards.filter(isNew);
 
-				sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
+			if(cards && cards.length > 0) {
+				console.log('⭐️ New: ' + cards.length);
+
+				sb.push('## ⭐️ New');
+
+				for (var i = 0; i < cards.length; i++) {
+					var c = cards[i];
+
+					sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
+
+				}
+
+				sb.push('\n');
+			}
+
+			//find "👍 Updated"
+			cards = list.cards.filter(isEnhancement);
+
+			if(cards && cards.length > 0) {
+				console.log('👍 Updated: ' + cards.length);
+
+				sb.push('## 👍 Updated');
+
+				for (var i = 0; i < cards.length; i++) {
+					var c = cards[i];
+
+					sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
+
+				}
+
+				sb.push('\n');
 
 			}
 
-			sb.push('\n');
-		}
+			//find "🐛 Fixed"
+			cards = list.cards.filter(isBug);
 
-		//find "👍 Updated"
-		cards = list.cards.filter(isEnhancement);
+			if(cards && cards.length > 0) {
+				console.log('🐛 Fixed: ' + cards.length);
 
-		if(cards && cards.length > 0) {
-			console.log('👍 Updated: ' + cards.length);
+				sb.push('## 🐛 Fixed');
 
-			sb.push('## 👍 Updated');
+				for (var i = 0; i < cards.length; i++) {
+					var c = cards[i];
 
-			for (var i = 0; i < cards.length; i++) {
-				var c = cards[i];
+					sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
 
-				sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
+				}
 
-			}
-
-			sb.push('\n');
-
-		}
-
-		//find "🐛 Fixed"
-		cards = list.cards.filter(isBug);
-
-		if(cards && cards.length > 0) {
-			console.log('🐛 Fixed: ' + cards.length);
-
-			sb.push('## 🐛 Fixed');
-
-			for (var i = 0; i < cards.length; i++) {
-				var c = cards[i];
-
-				sb.push('- [$name$]($url$)'.replace('$name$', c.name).replace('$url$', c.url));
+				sb.push('\n');
 
 			}
 
+			//⚠️ Known Issues
+			sb.push('## ⚠️ Known Issues');
 			sb.push('\n');
 
-		}
+			console.log(sb);
+			console.log('STEP END: copy to clipboard');
+			//console.log(sb.join('\n'))
+			//window.copy(sb.join('\n'));
+			//window.prompt('Copy your Release Notes', sb.join('\n'));
 
-		//⚠️ Known Issues
-		sb.push('## ⚠️ Known Issues');
-		sb.push('\n');
+			swal({
+				type: 'success',
+				title: 'All done!',
+				html:
+					'Your Release Notes:<textarea rows="10">' +
+						sb.join('\n') +
+					'</textarea>',
+				confirmButtonText: 'Lovely!',
+				showCancelButton: false
+			});
 
-		console.log(sb);
-		console.log('STEP END: copy to clipboard');
-		//console.log(sb.join('\n'))
-		//window.copy(sb.join('\n'));
-		//window.prompt('Copy your Release Notes', sb.join('\n'));
 
-	  swal({
-	    title: 'All done!',
-	    html:
-	      'Your Release Notes:<textarea>' +
-	        sb.join('\n') +
-	      '</textarea>',
-	    confirmButtonText: 'Lovely!',
-	    showCancelButton: false
-	  });
+		}).catch(swal.noop)
 
 	});
 
