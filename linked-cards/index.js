ed04151f03791a1326e8640e3c9ec937f11fa1d2
Anthony Vannier <anthony.vannier@ucaya.com>
2017-05-03 22:42:48 +0200

    Update "Linked cards" : Select QA board

--- linked-cards/index.js
+++ linked-cards/index.js
@@ -50,6 +50,33 @@
     document.body.appendChild(img);
   }
 
+
+  function searchCardFromBoard (jsonBoard, urlCard, sb){
+    for (var i = 0; i < jsonBoard.length; i++) {
+            var c = jsonBoard[i];
+            
+            var desc = c.desc;
+            //Find card url in description
+            if( desc.indexOf(urlCard)>-1){
+              sb.push('<a href="$url$">$name$</a><br/>'.replace('$name$', c.name).replace('$url$', c.url));
+            }
+
+            //Find card url in checklists items
+            for (var j = 0; j < c.checklists.length; j++) {
+              var checkItems = c.checklists[j].checkItems;
+              for (var k = 0; k < checkItems.length ; k++) {
+                var checkItem = checkItems[k];
+                if( checkItem.name.indexOf(urlCard)>-1){
+                  sb.push('<a href="$url$" target="_blank">$name$</a><br/>'.replace('$name$', c.name).replace('$url$', c.url));
+                }
+              }
+            }
+
+            //Find card url in attachments 
+            //TO DO
+        }
+  } 
+
   var start = function() {
 
     var parts = /\/c\/([^/]+)/.exec(document.location);
@@ -67,21 +94,59 @@
     var idCard = parts[1];
     var sbLinkedCards = [];
     var sbUrlCards = [];
+    var sbLinkedQACards = [];
     var allUrls = {};
+    var urlRegExp = /https:\/\/trello.com\/c\/[\w.,@?^=%&:\/~+#-]*[\w.,@?^=%&\/~+#-]/g;
 
     gaCollect('start', 'linked-cards', 'success');
       console.log('STEP 1: idCard: ' + idCard);
 
+    $.get('/1/batch?urls=/organizations/56d59f14e50d6ef4dcfb8dbd/boards,/cards/' + idCard, 
+    { fields: 'idBoard,name,desc,url,checklists', checklists: 'all' })
+    .sucess(function(jsonAll){
+    
+    var jsonAllBoards = jsonAll[0]["200"];
+    var jsonCard = jsonAll[0]["200"];
 
- $.get('/1/cards/' + idCard, { fields: 'idBoard,name,desc,url,checklists', checklists: 'all' })
-    .success(function(jsonCard){
+    var boardsSelector = '<select id="swal-board" class="swal2-input">';
+      for (var i = 0; i < jsonAllBoards.length; i++) {
+        var b = jsonAllBoards[i];
+        if( !b.closed){
+        //var selectedAttr = l.name.toLowerCase() === 'done' ? 'selected="selected"' : '';
+          boardsSelector += '<option value="' + b.id + '" ' + selectedAttr + '>' + b.name + '</option>';
+        }
+      }
+      boardsSelector += '</select>';
+      swal({
+        type: 'question',
+        title: 'Linked cards',
+        confirmButtonText: 'ðŸ–¨ Go!',
+        html:
+          'ðŸ—ƒ QA Board?' + boardsSelector ,
+        preConfirm: function () {
+          return new Promise(function (resolve) {
+            resolve([
+              $('#swal-board').val(),
+            ])
+          })
+        },
+        onOpen: function () {
+          $('#swal-board').focus()
+        }
+      }).then(function (result){
+        
+        var idBoardQA = result[0];
+        
+      $.get('/1/boards/' + idBoardQA + '/cards', { cards: 'open', card_fields: 'url,name,labels,desc,checklists', checklists: 'all' })
+  //  $.get('/1/cards/' + idCard, { fields: 'idBoard,name,desc,url,checklists', checklists: 'all' })
+      .success(function(jsonBoardQA){
 
       var urlCard = jsonCard.url;
       var idBoard = jsonCard.idBoard;
 
       //Find cards url in current  card description
       var descCard = jsonCard.desc;
-      var urlMatches = /https:\/\/trello.com\/c\/[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]/im.exec(descCard);
+      var urlMatches = descCard.match(urlRegExp);
       if(urlMatches){
             for (var i = 0; i < urlMatches.length; i++) {
               var infos = urlMatches[i].split('/');
@@ -98,7 +163,8 @@
         var checkItems = jsonCard.checklists[j].checkItems;
         for (var k = 0; k < checkItems.length ; k++) {
           var checkItem = checkItems[k];
-          urlMatches = /https:\/\/trello.com\/c\/[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]/im.exec(checkItem.name);
+          var name = checkItem.name;
+          urlMatches = name.match(urlRegExp);
           if(urlMatches){
             for (var i = 0; i < urlMatches.length; i++) {
               var infos = urlMatches[i].split('/');
@@ -114,43 +180,32 @@
 
       console.log('STEP 2: idBoard: ' + idBoard);
       //console.log('JSON : ' + JSON.stringify(jsonCard));
-
+    
     $.get('/1/boards/' + idBoard + '/cards', { cards: 'open', card_fields: 'url,name,labels,desc,checklists', checklists: 'all' })
       .success(function(jsonBoard){
 
-        for (var i = 0; i < jsonBoard.length; i++) {
-            var c = jsonBoard[i];
-            
-            var desc = c.desc;
-            //Find card url in description
-            if( desc.indexOf(urlCard)>-1){
-              sbLinkedCards.push('<a href="$url$">$name$</a><br/>'.replace('$name$', c.name).replace('$url$', c.url));
-            }
-
-            //Find card url in checklists items
-            for (var j = 0; j < c.checklists.length; j++) {
-              var checkItems = c.checklists[j].checkItems;
-              for (var k = 0; k < checkItems.length ; k++) {
-                var checkItem = checkItems[k];
-                if( checkItem.name.indexOf(urlCard)>-1){
-                  sbLinkedCards.push('<a href="$url$" target="_blank">$name$</a><br/>'.replace('$name$', c.name).replace('$url$', c.url));
-                }
-              }
-            }
-        }
+        searchCardFromBoard (jsonBoard, urlCard, sbLinkedCards);
+        searchCardFromBoard (jsonBoardQA, urlCard, sbLinkedQACards);
     
       //console.log(sb);
       console.log('STEP END: linked-cards');
 
       var htmlUrlCards = sbUrlCards.length>0 ? '<div style="text-align:left"><p>Referenced cards</p>'+ sbUrlCards.join('\n') +'</div>' : '';
-
-      var htmlLinkedCards = sbLinkedCards.length>0 ? '<div style="text-align:left"><p>Your card is linked to</p>'+ sbLinkedCards.join('\n') +'</div>' : '';
+      var htmlLinkedCards = sbLinkedCards.length>0 ? '<div style="text-align:left"><p>Referenced by</p>'+ sbLinkedCards.join('\n') +'</div>' : '';
+      var htmlLinkedQACards = sbLinkedQACards.length>0 ? '<div style="text-align:left"><p>QA cards</p>'+ sbLinkedQACards.join('\n') +'</div>' : '';
       
       var htmlContent = htmlUrlCards;
+
       if( htmlContent && htmlLinkedCards )
         htmlContent += "<br/>" + htmlLinkedCards;
       else if( htmlLinkedCards )
         htmlContent = htmlLinkedCards;
+
+      if( htmlContent && htmlLinkedQACards )
+        htmlContent += "<br/>" + htmlLinkedQACards;
+      else if( htmlLinkedQACards )
+        htmlContent = htmlLinkedQACards;
+      
 
         swal({
           type: 'success',
@@ -164,10 +219,15 @@
           });
       });
 
-    });
+      });
+
+      }).catch(swal.noop)
+
+    });     
+
 
   };
-
+  
   start();
 
 })();
